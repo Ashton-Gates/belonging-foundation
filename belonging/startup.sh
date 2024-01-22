d611d556e5c287f45127ac1d8ec11e56d9c2155c
Botnetgroup <ashkin@botnet.group>
2024-01-22 00:11:45 -0500

    DId lots of adding to Strip and migration

---
+++ belonging/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Apply database migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Start Gunicorn
+gunicorn --bind=0.0.0.0 --timeout 600 belonging.wsgi
