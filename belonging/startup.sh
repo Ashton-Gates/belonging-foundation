cd32783f81e365912e19f93ebbc724a70db6a97a
Botnetgroup <ashkin@botnet.group>
2024-01-24 10:27:36 -0500

    adjusted startup.sh for the wsgi configurations. Binded it to 'A' record type in Gunicorn

--- belonging/startup.sh
+++ belonging/startup.sh
@@ -10,4 +10,4 @@ python manage.py migrate
 python manage.py collectstatic --no-input
 
 # Start Gunicorn
-gunicorn --bind=0.0.0.0 --timeout 600 belonging.wsgi:application
+gunicorn --bind=20.119.16.56:80 --timeout 600 belonging.wsgi:application
