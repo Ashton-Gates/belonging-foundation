55796866719783ca576960af90636d06e0b533ea
Botnetgroup <ashkin@botnet.group>
2024-02-23 12:51:30 -0500

    Added ODBC driver installs to the .yml deployment workflow

--- test_db_connection.py
+++ test_db_connection.py
@@ -3,13 +3,17 @@ import sys
 import pyodbc
 
 def test_database_connection():
-    connection_string = os.environ.get('DATABASE_CONNECTION_STRING')
+    connection_string = ('Driver={ODBC Driver 18 for SQL Server};'
+                             'Server=tcp:belonging-server.database.windows.net,1433;'
+                             'Database=belonging-prod-database;'
+                             'UID=belo-admin;'
+                             'PWD=H1ng3isgro$$;')
     if not connection_string:
         print("DATABASE_CONNECTION_STRING environment variable is not set.")
         sys.exit(1)
     
     try:
-        with pyodbc.connect(connection_string, timeout=10) as conn:
+        with pyodbc.connect(connection_string, timeout=30) as conn:
             print("Successfully connected to the database.")
     except Exception as e:
         print(f"Failed to connect to the database: {e}")
